0662d4d135500897ca9537af5fb010b56b992c5e
Judit <tolnai.judit12@gmail.com>
2017-06-23 11:46:36 +0200

    exercise 1,2 -> ready

---
+++ DictionaryCollections/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable myHashTable = new Hashtable();
+        }
+    }
+}
--- SequentialCollections/Program.cs
+++ SequentialCollections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,36 @@ namespace SequentialCollections
     {
         static void Main(string[] args)
         {
+            Queue myQueue = new Queue();
+            myQueue.Enqueue("First");
+            myQueue.Enqueue("Second");
+            myQueue.Enqueue("Third");
+            myQueue.Enqueue("Fourth");
+            myQueue.Enqueue(1);
+            myQueue.Enqueue(true);
+
+            while (myQueue.Count != 0)
+            {
+                object obj = myQueue.Dequeue();
+
+                Console.WriteLine("From Queue: {0}", obj);
+            }
+
+            Stack myStack = new Stack();
+            myStack.Push("First");
+            myStack.Push("Second");
+            myStack.Push("Third");
+            myStack.Push("Fourth");
+            myStack.Push(1);
+            myStack.Push(true);
+
+            while (myStack.Count > 0)
+            {
+                object obj = myStack.Pop();
+                Console.WriteLine("From Stack: {0}", obj);
+            }
+
+
         }
     }
 }
